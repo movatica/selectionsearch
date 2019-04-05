b9ed0cf241922df4919071f1e9f366ec19cc4b02
movatica <c0d3@movatica.com>
2019-04-05 15:32:13 +0200

    use jQuery.param() to build search link
    + use jQuery.param() instead of string concatenation and encodeURIComponent()
    + use const everywhere, because we do not even change a single variable here

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 jQuery(function(){
     'use strict';
 
-    var tooltip = jQuery('#selectionsearch__tooltip');
+    const tooltip = jQuery('#selectionsearch__tooltip');
     if (!tooltip[0]) return;
 
     // mouseup callback for content div
     jQuery('#dokuwiki__content').mouseup(function(event){
         tooltip.hide();
 
-        var query = window.getSelection().toString().trim();
+        const query = window.getSelection().toString().trim();
         if (query.length < JSINFO.selectionsearch_minlength) return;
 
-        var href = '?id='+JSINFO.id+'&do=search&q='+encodeURIComponent(query);
+        const href = '?' + jQuery.param({ id: JSINFO.id, do: 'search', q: query });
         jQuery('#selectionsearch__link').attr('href', href);
 
         tooltip.css({top: event.pageY+JSINFO.selectionsearch_ycorr,
